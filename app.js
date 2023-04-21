83149808c36be26d0fc378bb27ffc15c8f1e1a84
Sasha Bolgova <aleks.kostyleva@yandex.ru>
2023-04-21 21:26:59 +0300

    refactor: testing users

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { cardRouter } = require('./routes/cards')
 // Слушаем 3000 порт
 const { PORT = 3000, LOCALHOST = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
-app.use(userRouter);
+
 app.use((req, res, next) => {
   req.user = {
     _id: '64427ec973e5a1b823ac9965'
@@ -19,11 +19,11 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 mongoose.connect(LOCALHOST, {
  useNewUrlParser: true
 });
 
+app.use(userRouter);
 app.use(cardRouter);
 
 app.use('*', (req, res) => res.status(404).send({message:'Страница не найдена'}));
--- controllers/cards.js
+++ controllers/cards.js
@@ -17,7 +17,6 @@ module.exports.createCard = (req, res) => {
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-  .populate(['owner', 'likes'])
     .then((cards) => res.status(200).send({ data: cards }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
@@ -34,7 +33,7 @@ module.exports.deleteCard = (req, res) => {
       } else if (err.message === 'Карточка не найдена') {
         res.status(404).send({ message: 'Карточка не найдена'});
       } else {
-        res.status(500).send({ message: 'Произошла ошибка' });
+        res.status(404).send({ message: 'Карточка не найдена'});
       }
     });
 };
@@ -53,7 +52,7 @@ module.exports.likeCard = (req, res) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
       } else {
-        res.status(500).send({ message: 'Произошла ошибка' });
+        res.status(404).send({ message: 'Карточка не найдена' });
       }
     });
 };
