c0783a9d505a772c63df501a0290e8efb26aa28f
Sasha Bolgova <aleks.kostyleva@yandex.ru>
2023-04-23 22:35:23 +0300

    refactor: отправка на проверку

--- controllers/users.js
+++ controllers/users.js
@@ -16,10 +16,8 @@ module.exports.getUserId = (req, res) => {
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
-      } else if (err.name === 'Пользователь не найден') {
-        res.status(404).send({ message: 'Пользователь не найден' });
       } else {
-        res.status(500).send({ message: 'Произошла ошибка' });
+        res.status(404).send({ message: 'Пользователь не найден' });
       }
     });
 };
